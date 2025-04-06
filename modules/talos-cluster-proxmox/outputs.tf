8ee23000d78a12b2fe5eb07e797a2e6813782d8e
Matus Svihra <msvihra@qualityunit.com>
2025-04-06 21:39:06 +0200

    feat: Add new outputs

--- modules/talos-cluster-proxmox/outputs.tf
+++ modules/talos-cluster-proxmox/outputs.tf
@@ -7,3 +7,11 @@ output "kubeconfig" {
   value     = talos_cluster_kubeconfig.this.kubeconfig_raw
   sensitive = true
 }
+
+output "talos_cluster_name" {
+  value = var.talos.cluster_name
+}
+
+output "talos_cluster_version" {
+  value = var.talos.version
+}
