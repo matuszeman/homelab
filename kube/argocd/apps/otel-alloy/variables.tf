af0fb8907c56d04ec2b40f9aabdcf53ba5383bb0
Matus Zeman <matus.zeman@gmail.com>
2025-04-18 20:06:48 +0200

    feat(otel-alloy): otel collector / beyla
    Works with grafana cloud.

---
+++ kube/argocd/apps/otel-alloy/variables.tf
@@ -0,0 +1,15 @@
+variable "argocd" {}
+variable "namespace" {
+  type = string
+}
+variable "release" {}
+variable "cluster_name" {}
+variable "sealed_secrets" {
+  type = object({
+    access_token = string
+    prometheus_username = string
+    loki_username = string
+    tempo_username = string
+    pyroscope_username = string
+  })
+}
