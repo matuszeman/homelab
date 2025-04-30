f2d36cf13d20dd67292f0ab14a483ee657841a71
Matus Zeman <matus.zeman@gmail.com>
2025-04-30 08:58:30 +0200

    feat(modules): zerotier member authorized flag

--- modules/network-zerotier-bridged/static-assignments.tf
+++ modules/network-zerotier-bridged/static-assignments.tf
@@ -8,12 +8,12 @@ resource "zerotier_member" "this" {
   for_each = local.static_ips
 
   name                    = "static-${each.key}"
-  member_id               = each.value.zerotier_member
+  member_id               = each.value.zerotier_member.id
   network_id              = zerotier_network.this.id
-  authorized              = true
+  authorized              = each.value.zerotier_member.authorized
   description             = "tf"
   hidden                  = false
-  allow_ethernet_bridging = true
+  allow_ethernet_bridging = false
   no_auto_assign_ips      = true
   ip_assignments          = [each.value.address]
 }
