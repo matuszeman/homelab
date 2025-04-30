fa40de2131e0b3b536d39e19de27d2e0d777c211
Matus Zeman <matus.zeman@gmail.com>
2025-04-30 08:18:18 +0200

    static assignements

--- modules/network-zerotier-bridged/static-assignments.tf
+++ modules/network-zerotier-bridged/static-assignments.tf
@@ -4,15 +4,15 @@ locals {
   }
 }
 
-# resource "zerotier_member" "this" {
-#   for_each = local.static_ips
-#
-#   name                    = "${each.key}-static"
-#   member_id               = each.value.zerotier_member
-#   network_id              = zerotier_network.this.id
-#   description             = "tf"
-#   hidden                  = false
-#   allow_ethernet_bridging = true
-#   no_auto_assign_ips      = true
-#   ip_assignments          = [each.value.address]
-# }
+resource "zerotier_member" "this" {
+  for_each = local.static_ips
+
+  name                    = "${each.key}-static"
+  member_id               = each.value.zerotier_member
+  network_id              = zerotier_network.this.id
+  description             = "tf"
+  hidden                  = false
+  allow_ethernet_bridging = true
+  no_auto_assign_ips      = true
+  ip_assignments          = [each.value.address]
+}
